54dc528e941b07a19bdfe0b9a898819c301c3923
Fredrik F. Lindhagen <fred.lindh96@gmail.com>
2020-04-09 13:05:20 +0200

    Use update /game-modes endpoint

--- src/state/gameMode/api.js
+++ src/state/gameMode/api.js
@@ -3,4 +3,4 @@ import { callGet } from '../../utils/api';
 const BASE_URL = 'http://localhost:3001/v1';
 
 // eslint-disable-next-line import/prefer-default-export
-export const fetchAllGameModes = () => callGet(`${BASE_URL}/gamemode/`);
+export const fetchAllGameModes = () => callGet(`${BASE_URL}/game-modes/`);
